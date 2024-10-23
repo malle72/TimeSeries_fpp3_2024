9218c54121bb423578cec3082d99804d2bd748a3
malle72 <42781131+malle72@users.noreply.github.com>
2024-10-23 09:07:50 -0500

    Chapter 7 initial commit

---
+++ Chapter 7/cptr 7 practice.R
@@ -0,0 +1,103 @@
+library(fpp3)
+
+# ==== 7.1 The Linear Model ====
+
+# Code from video
+fit_cons <- us_change |>
+  model(lm = TSLM(Consumption ~ Income))
+
+report(fit_cons)
+
+us_change |>
+  pivot_longer(-Quarter) |>
+  ggplot(aes(Quarter, value, colour = name)) +
+  geom_line() +
+  facet_grid(name ~ ., scales = "free_y") +
+  guides(colour = "none")
+
+us_change |>
+  GGally::ggpairs(columns = 2:6)
+
+# Textbook Code
+
+# Line plot of Consumption and Income
+us_change |>
+  pivot_longer(c(Consumption, Income), names_to="Series") |>
+  autoplot(value) +
+  labs(y = "% change")
+
+# Scatter plot with Regression Line
+us_change |>
+  ggplot(aes(x = Income, y = Consumption)) +
+  labs(y = "Consumption (quarterly % change)",
+       x = "Income (quarterly % change)") +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# Make model and get diagnostics
+us_change |>
+  model(TSLM(Consumption ~ Income)) |>
+  report()
+
+# Plotting multiple variables 
+us_change |>
+  select(-Consumption, -Income) |>
+  pivot_longer(-Quarter) |>
+  ggplot(aes(Quarter, value, colour = name)) +
+  geom_line() +
+  facet_grid(name ~ ., scales = "free_y") +
+  guides(colour = "none") +
+  labs(y="% change")
+
+# Correlation Matrix
+us_change |>
+  GGally::ggpairs(columns = 2:6)
+
+
+# ==== 7.2 Least Squares Estimation ====
+fit_consMR <-us_change |>
+  model(lm=TSLM(Consumption ~ Income + Production + Unemployment + Savings))
+
+report(fit_consMR)
+
+augment(fit_consMR) |>
+  ggplot(aes(x = Quarter)) +
+  geom_line(aes(y = Consumption, colour = "Data")) +
+  geom_line(aes(y = .fitted, colour = "Fitted")) +
+  labs(y = NULL,
+       title = "Percent change in US consumption expenditure"
+  ) +
+  scale_colour_manual(values=c(Data="black",Fitted="#D55E00")) +
+  guides(colour = guide_legend(title = NULL))
+
+augment(fit_consMR) |>
+  ggplot(aes(x = Consumption, y = .fitted)) +
+  geom_point() +
+  labs(
+    y = "Fitted (predicted values)",
+    x = "Data (actual values)",
+    title = "Percent change in US consumption expenditure"
+  ) +
+  geom_abline(intercept = 0, slope = 1)
+
+
+# ==== 7.3 Evaluating the Regression Model ====
+
+gg_tsresiduals(fit_consMR)
+
+augment(fit_consMR) |>            # augment adds fitted values and residuals (response and innov)
+  features(.innov, ljung_box, lag = 10)
+
+
+us_change |>
+  left_join(residuals(fit_consMR), by = "Quarter") |>
+  pivot_longer(Income:Unemployment,               # pivots data such that there is a row per regressor/value
+               names_to = "regressor", values_to = "x") |>  
+  ggplot(aes(x = x, y = .resid)) +
+  geom_point() +
+  facet_wrap(. ~ regressor, scales = "free_x") +
+  labs(y = "Residuals", x = "")
+
+# ==== 7.4 Some Useful Predictors ====
+
+
